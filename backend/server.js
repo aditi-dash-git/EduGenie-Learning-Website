8ada527357b3d6e623d2daaa10a8147f91541b0b
Aditi <dashaditi8@gmail.com>
2026-05-04 20:58:16 +0530

    Fixed payment

--- backend/server.js
+++ backend/server.js
@@ -164,7 +164,7 @@ app.use(
 app.use(express.urlencoded({ extended:true }));
 
 /* Static uploads */
-app.use('/uploads', express.static(path.join(__dirname,'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
 app.use(clerkMiddleware());
 
