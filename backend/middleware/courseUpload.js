bdd672a53e4b21eb748cf03c08f4e6a9f9519577
Aditi <dashaditi8@gmail.com>
2026-05-04 20:53:21 +0530

    Fixed payment

--- backend/middleware/courseUpload.js
+++ backend/middleware/courseUpload.js
@@ -1,11 +1,16 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: function(req,file,cb){
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: function(req,file,cb){
+//     cb(null, Date.now() + "-" + file.originalname);
+//   }
+// });
+
+const storage = multer.memoryStorage();
+
+
+
 
 const upload = multer({ storage });
 
